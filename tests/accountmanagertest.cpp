d011ac273744fe6508b96497f6e75ef4c0017315
Nate Rauh <NRauh@users.noreply.github.com>
2016-02-09 19:51:32 -0600

    Initial Commit

---
+++ tests/accountmanagertest.cpp
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+
+TEST_CASE("Can create budget/save files", "[createBudget]") {
+  SECTION("A path is given, it creates a .mbgt file") {
+      REQUIRE(true == true);
+  }
+}
+
+TEST_CASE("Can add checking account", "[addChecking]") {
+  SECTION("The details are given, a table for a checking account is created") {
+  }
+}
