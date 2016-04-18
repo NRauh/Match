e3cf6d0144b8f3955896e39b2142f06c63c6f8df
Nate Rauh <NRauh@users.noreply.github.com>
2016-04-17 23:25:04 -0500

    Made common variables for tests global
    Moved some variables like filePath, and account to be accessible to all
    tests.
    They were declared a lot, so it was to reduce lines, and possibly
    marignally improve test speed.
    Also removed accManager variable since it wasn't used.

--- tests/accounttest.cpp
+++ tests/accounttest.cpp
@@ -13,14 +13,13 @@
 QString fooCuId;
 QString testTransactionId;
 
+Account account;
+QUrl filePath = QUrl::fromLocalFile("Foo Budget.mbgt");
+QDate transactionDate = QDate(2016, 2, 29);
+
 TEST_CASE("Can add checking accounts", "[addChecking]") {
     SECTION("File path, account name, balance, and balance date are given") {
-        Account account;
-        AccountManager accManager;
-        QUrl filePath = QUrl::fromLocalFile("Foo Budget.mbgt");
-        QDate balanceDate = QDate(2016, 2, 29);
-
-        account.addChecking(filePath, "Foo CU", 80000, balanceDate);
+        account.addChecking(filePath, "Foo CU", 80000, transactionDate);
 
         io::sqlite::db budget("Foo Budget.mbgt");
         io::sqlite::stmt query(budget, "SELECT accountName, balance FROM accounts");
@@ -43,11 +42,6 @@ TEST_CASE("Can add checking accounts", "[addChecking]") {
 
 TEST_CASE("Can add transactions to account", "[addTransaction]") {
     SECTION("Given file path, account ID, date, payee, if outflow, amount, and note") {
-        Account account;
-        AccountManager accManager;
-        QUrl filePath = QUrl::fromLocalFile("Foo Budget.mbgt");
-        QDate transactionDate = QDate(2016, 2, 29);
-
         account.addTransaction(filePath, 1, transactionDate, "Caffe Nero", true, 125, "Espresso");
         io::sqlite::db budget("Foo Budget.mbgt");
         io::sqlite::stmt query(budget, "SELECT toAccount, transactionDate, payee, amount, outflow, note FROM transactions WHERE id == 2");
@@ -68,11 +62,6 @@ TEST_CASE("Can add transactions to account", "[addTransaction]") {
     }
 
     SECTION("If outflow is false, then it's income and should be added") {
-        Account account;
-        AccountManager accManager;
-        QUrl filePath = QUrl::fromLocalFile("Foo Budget.mbgt");
-        QDate transactionDate = QDate(2016, 2, 29);
-
         account.addTransaction(filePath, 1, transactionDate, "Tip", false, 1000, "");
         io::sqlite::db budget("Foo Budget.mbgt");
         io::sqlite::stmt query(budget, "SELECT balance FROM accounts WHERE id == 1");
@@ -85,8 +74,6 @@ TEST_CASE("Can add transactions to account", "[addTransaction]") {
 
 TEST_CASE("Can update a transaction", "[updateTransaction]") {
     SECTION("Filepath, transaction ID, date, payee, outflow, amount, and note") {
-        Account account;
-        QUrl filePath = QUrl::fromLocalFile("Foo Budget.mbgt");
         QDate transactionDate = QDate(2016, 2, 28);
 
         account.updateTransaction(filePath, 3, transactionDate, "Tips", true, 1100, "Tip I guess");
@@ -104,10 +91,6 @@ TEST_CASE("Can update a transaction", "[updateTransaction]") {
     }
 
     SECTION("It shows on the balance") {
-        Account account;
-        QUrl filePath = QUrl::fromLocalFile("Foo Budget.mbgt");
-        QDate transactionDate = QDate(2016, 2, 29);
-
         io::sqlite::db budget("Foo Budget.mbgt");
         io::sqlite::stmt query(budget, "SELECT balance FROM accounts WHERE id == 1");
         while (query.step()) {
@@ -120,10 +103,6 @@ TEST_CASE("Can update a transaction", "[updateTransaction]") {
 
 TEST_CASE("Can delete transactions", "[deleteTransaction]") {
     SECTION("Filepath, and transaction ID will delete that transaction") {
-        Account account;
-        QUrl filePath = QUrl::fromLocalFile("Foo Budget.mbgt");
-        QDate transactionDate = QDate(2016, 2, 29);
-
         account.deleteTransaction(filePath, 3);
 
         io::sqlite::db budget("Foo Budget.mbgt");
@@ -149,9 +128,6 @@ TEST_CASE("Can delete transactions", "[deleteTransaction]") {
 
 TEST_CASE("Can get a list of accounts and balances", "[getAccountList]") {
     SECTION("Give file path, returns name, balance, and id") {
-        Account account;
-        QUrl filePath = QUrl::fromLocalFile("Foo Budget.mbgt");
-
         QJsonObject accountList = account.getAccountList(filePath);
         REQUIRE(accountList["balance"] == "798.75");
         REQUIRE(accountList["accounts"].toArray()[0].toObject()["accountId"] == 1);
@@ -162,9 +138,6 @@ TEST_CASE("Can get a list of accounts and balances", "[getAccountList]") {
 
 TEST_CASE("Can get list of transactions and balance for account", "[getTransactions]") {
     SECTION("Filepath, and accountId returns balance and array of transactions") {
-        Account account;
-        QUrl filePath = QUrl::fromLocalFile("Foo Budget.mbgt");
-
         QJsonObject transactions = account.getTransactions(filePath, 1);
 
         REQUIRE(transactions["balance"] == "798.75");
